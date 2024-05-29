b66577ca0346b8abc0cf4d7dd8642c3d379bb7a3
JesperFrid <elev.jesper.frid@skola.lund.se>
2024-05-29 13:12:12 +0200

    prov3

---
+++ prov/javascript/provscript.js
@@ -0,0 +1,6 @@
+function backcolor()
+{
+    document.getElementById("title").style.backgroundColor = "#88dd89";
+    document.getElementById("description").style.backgroundColor = "#add7e7"
+    document.getElementById("read").innerHTML = "Read enough";
+}
